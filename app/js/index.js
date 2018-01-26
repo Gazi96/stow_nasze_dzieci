65a313e4ee4003242234cc6e5dd28606a9e8c92d
Rafał Gazda <gazi1996@o2.pl>
2018-01-27 00:08:40 +0100

    mobile to 800px

--- app/js/index.js
+++ app/js/index.js
@@ -8,8 +8,6 @@ function drop()
     nav.classList.toggle('nav--drop');
 }
 
-
-
 //slider
 var slider = document.getElementsByClassName("slider__box")[0];
 var sliderList = document.getElementsByClassName("slider__container");
@@ -17,27 +15,36 @@ var sliderList = document.getElementsByClassName("slider__container");
 var prev = document.getElementsByClassName("prev")[0];
 var next = document.getElementsByClassName("next")[0];
 
+var sliderContent = document.getElementsByClassName("slidercontent");
+var buttonSlider = document.getElementsByClassName("section__button--slider")[0];
+var trapezoid = document.getElementsByClassName("trapezoid__circle");
+
 prev.addEventListener("click", myPrev);
 next.addEventListener("click", myNext);
 
 var current = 0;
 
+backgroundColor();
+content();
+
 function myPrev()
 {
+    
+    console.log(current);
+    
     if(current>0)
     {        
         current--;
         slider.style.left = -100*current + "vw";
     }
     else
-    {        
-        /*var content = document.getElementsByClassName("slider__container")[2];
-        var parent = content.parentNode;
-        parent.insertBefore(content, parent.firstChild);*/
-        
+    {                
         slider.style.left = (-100*(sliderList.length-1)) + "vw";
         current = sliderList.length-1;
     }
+    
+    backgroundColor();
+    content();
     
     clearTimeout(myTimer);
     myTimer = setTimeout(showSlides, 3000);
@@ -45,25 +52,25 @@ function myPrev()
 
 function myNext()
 {   
+    console.log(current);
+    
     if(current<=sliderList.length-2)
     {
         current++;
         slider.style.left = -100*current + "vw";
     }
     else
-    {
-        /*var content = document.getElementsByClassName("slider__container")[0];
-        var parent = content.parentNode;
-        parent.insertBefore(content, parent.lastChild);*/
-        
+    {        
         slider.style.left = "0vw";
         current = 0;
     }
     
+    backgroundColor();
+    content();
+    
     clearTimeout(myTimer);
     myTimer = setTimeout(showSlides, 3000);
-    
-    
+
 }
 
 var myTimer = setTimeout(showSlides, 3000);
@@ -86,8 +93,178 @@ function showSlides()
         current = 0;
     }
     
+    backgroundColor();
+    content();
+    
     myTimer = setTimeout(showSlides, 3000);
 }
+
+
+
+var myFunction = function() {
+    for(var i=0; i<trapezoid.length; i++){
+        if(trapezoid[i].classList.remove("current"));
+    }
+    
+    this.classList += " current";
+    for(var i=0; i<trapezoid.length; i++){
+        if(trapezoid[i].classList.contains("current"))
+        {
+            if(i>current)
+            {
+                current = i-1;
+                
+                if(current<=sliderList.length-2)
+                {
+                    current++;
+                    slider.style.left = -100*current + "vw";
+                }
+                else
+                {        
+                    slider.style.left = "0vw";
+                }
+                
+                backgroundColor();
+                
+                clearTimeout(myTimer);
+                myTimer = setTimeout(showSlides, 3000);
+            }
+            else
+            {
+                current = i+1;
+                
+                console.log(current);
+                
+                if(current>0)
+                {
+                    current--;
+                    slider.style.left = -100*current + "vw";
+                }
+                else
+                {                
+                    slider.style.left = (-100*(sliderList.length-1)) + "vw";
+                }
+                
+                backgroundColor();
+
+                clearTimeout(myTimer);
+                myTimer = setTimeout(showSlides, 3000);
+            }
+        }
+    }
+};
+
+
+
+for (var i = 0; i < trapezoid.length; i++) {
+    trapezoid[i].addEventListener('click', myFunction, false);
+}
+
+function backgroundColor()
+{
+    for(var i=0; i<trapezoid.length; i++){
+            trapezoid[i].style.backgroundColor = "white";
+    }
+    
+    if(current==0)
+    {
+        trapezoid[current].style.backgroundColor = "rgb(255, 180, 0)";
+        buttonSlider.style.backgroundColor =  "rgb(255, 180, 0)";
+    }
+    if(current==1)
+    {
+        trapezoid[current].style.backgroundColor = "rgb(0, 166, 237)";
+        buttonSlider.style.backgroundColor = "rgb(0, 166, 237)";
+    }
+    if(current==2)
+    {
+        trapezoid[current].style.backgroundColor = "rgb(127, 184, 0)";
+        buttonSlider.style.backgroundColor = "rgb(127, 184, 0)";
+    }
+    if(current==3)
+    {
+        trapezoid[current].style.backgroundColor = "rgb(13, 44, 84)";
+        buttonSlider.style.backgroundColor = "rgb(13, 44, 84)";
+    }
+    if(current==4)
+    {
+        trapezoid[current].style.backgroundColor = "rgb(246, 81, 29)";
+        buttonSlider.style.backgroundColor = "rgb(246, 81, 29)";
+    }
+}
+
+function content()
+{
+    for(var i=0; i<sliderContent.length; i++)
+    {
+        sliderContent[i].style.display = "none";   
+    }
+    
+    sliderContent[current].style.display = "block";
+}
+
+
+
+
+
+/*var trapezoid = document.getElementsByClassName("trapezoid__circle");
+
+for (var i = 0; i < trapezoid.length; i++) {
+    trapezoid[i].addEventListener('click', myFunction, false);
+}
+
+var myFunction = function() {
+    console.log(działa);
+    var attribute = this.getAttribute("data-myattribute");
+    alert(attribute);
+};*/
+
+
+
+/*for(var i=0; i<trapezoid.length; i++)
+{
+    trapezoid[i].addEventListener("click", function current(e)
+    {
+        console.log(current);
+        if(current>i)
+        {
+            console.log(i);
+            current = i;
+            myNext();
+        }
+        else
+        {
+            console.log(i);
+            current = i;
+            myPrev();
+        }
+        
+    });
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 var slider__carousel = document.getElementsByClassName('slider--carousel');
