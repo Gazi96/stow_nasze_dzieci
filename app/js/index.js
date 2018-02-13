80752e872509597b8f8155bf3b9cbbcb959baeb1
Rafał Gazda <gazi1996@o2.pl>
2018-02-13 22:38:15 +0100

    fix header

--- app/js/index.js
+++ app/js/index.js
@@ -14,7 +14,7 @@ function drop()
 
 $(function() {
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) {
+        if ($(this).scrollTop() > 40) {
              $('.header').addClass('fix');
         } else {
              $('.header').removeClass('fix');
